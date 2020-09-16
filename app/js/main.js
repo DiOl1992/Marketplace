2fe88b60312953e523b5993c7c522a61df0b7bd2
Diana <dianagerasimova1992@gmail.com>
2020-09-16 03:38:24 +0300

    added 1/2 newest release products

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,16 @@ $(function () {
 
   $(".rate-star").rateYo({
     rating: 4.5,
-    starWidth: "12px",
+    starWidth: "17px",
     readOnly: true
+  });
+
+  $('.featured-products__inner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>'
   });
 
 });
